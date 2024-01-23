ebb203beb96b1a250e91dc4ca68cc65462e05204
shravan <shravan@nextwaretech.net>
2024-01-23 21:53:30 +0530

    edited the asgi file in the application

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -1,26 +1,17 @@
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+# mysite/asgi.py
 import os
 
-import chat.routing
-
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels.auth import AuthMiddlewareStack
-
 from django.core.asgi import get_asgi_application
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
